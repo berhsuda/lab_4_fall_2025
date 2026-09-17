05979844e6dffc9bfb6c13a786ce289f4ed89b8e
David Hsu <berhsuda@gmail.com>
2026-09-17 14:55:42 -0700

    added rviz support

--- lab_4.launch.py
+++ lab_4.launch.py
@@ -55,9 +55,12 @@ def generate_launch_description():
             "lab_4.yaml",
         ]
     )
-    # rviz_config_file = PathJoinSubstitution(
-    #     [FindPackageShare("ros2_control_demo_example_1"), "rviz", "rrbot.rviz"]
-    # )
+    rviz_config_file = PathJoinSubstitution(
+        [
+            ThisLaunchFileDir(),
+            "lab_4.rviz",
+        ]
+    )
 
     control_node = Node(
         package="controller_manager",
@@ -71,13 +74,13 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
     )
-    # rviz_node = Node(
-    #     package="rviz2",
-    #     executable="rviz2",
-    #     name="rviz2",
-    #     output="log",
-    #     arguments=["-d", rviz_config_file],
-    # )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
 #    joy_node = Node(
 #        package="joy_linux",
 #        executable="joy_linux_node",
@@ -103,12 +106,12 @@ def generate_launch_description():
     )
 
     # Delay rviz start after `joint_state_broadcaster`
-    # delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=joint_state_broadcaster_spawner,
-    #         on_exit=[rviz_node],
-    #     )
-    # )
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
 
     # delay_joint_state_broadcaster_spawner_after_control_node = RegisterEventHandler(
     #     event_handler=OnProcessExit(
@@ -131,7 +134,7 @@ def generate_launch_description():
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
         imu_sensor_broadcaster_spawner,
-        # delay_rviz_after_joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
     ]
 
